0abcd31ab6dbe05983fba0332077e9e49f110a77
Sebazzz <triadsebas@gmail.com>
2015-01-02 21:16:31 +0100

    Eventing

---
+++ src/EntityProfiler.Common/Events/IMessageEventSubscriber.cs
@@ -0,0 +1,18 @@
+namespace EntityProfiler.Common.Events {
+    /// <summary>
+    /// Defines the interface message event listeners should implement
+    /// </summary>
+    internal interface IMessageEventSubscriber {
+        /// <summary>
+        /// Occurs when a message is received
+        /// </summary>
+        /// <param name="event"></param>
+        void OnReceived(MessageEvent @event);
+
+        /// <summary>
+        /// Occurs before a message is send
+        /// </summary>
+        /// <param name="event"></param>
+        void OnSending(MessageEvent @event);
+    }
+}
---
+++ src/EntityProfiler.Common/Events/MessageEvent.cs
@@ -0,0 +1,22 @@
+﻿namespace EntityProfiler.Common.Events {
+    using System.Diagnostics;
+    using Protocol.Objects;
+
+    /// <summary>
+    /// Represents event data for when a message is received
+    /// </summary>
+    internal class MessageEvent {
+        private readonly Message _message;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// </summary>
+        public MessageEvent(Message message) {
+            this._message = message;
+        }
+
+        public Message Message {
+            [DebuggerStepThrough] get { return this._message; }
+        }
+    }
+}
