fad1f7b4996e79cbfa3ea54d86ac903cae2406f8
Sebazzz <triadsebas@gmail.com>
2015-01-02 22:45:24 +0100

    Wrote first unit test expectations for the serialization API

---
+++ test/EntityProfiler.Tests.Unit/Common/Protocol/Serializer/JsonMessageDeserializerTests.cs
@@ -0,0 +1,39 @@
+namespace EntityProfiler.Tests.Unit.Common.Protocol.Serializer {
+    using System;
+    using System.IO;
+    using EntityProfiler.Common.Protocol.Serializer;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class JsonMessageDeserializerTests {
+        [Test]
+        public void JsonMessageDeerializer_WhenGivenNullTypeResolver_ThrowsArgumentNullException() {
+            // given
+            IMessageTypeResolver resolver = null;
+            TextReader textReader = new StringReader(String.Empty);
+
+            // when
+            ArgumentNullException exception =
+                Assert.Throws<ArgumentNullException>(
+                    () => new JsonMessageDeserializer(resolver, textReader));
+
+            // then
+            Assert.That(() => exception.ParamName, Is.EqualTo("typeResolver"));
+        }
+
+        [Test]
+        public void JsonMessageDeserializer_WhenGivenNullTextWriter_ThrowsArgumentNullException() {
+            // given
+            IMessageTypeResolver resolver = new UnitTestMessageTypeResolver();
+            TextReader textReader = null;
+
+            // when
+            ArgumentNullException exception =
+                Assert.Throws<ArgumentNullException>(
+                    () => new JsonMessageDeserializer(resolver, textReader));
+
+            // then
+            Assert.That(() => exception.ParamName, Is.EqualTo("textWriter"));
+        }
+    }
+}
---
+++ test/EntityProfiler.Tests.Unit/Common/Protocol/Serializer/JsonMessageSerializerTests.cs
@@ -0,0 +1,39 @@
+﻿namespace EntityProfiler.Tests.Unit.Common.Protocol.Serializer {
+    using System;
+    using System.IO;
+    using EntityProfiler.Common.Protocol.Serializer;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class JsonMessageSerializerTests {
+        [Test]
+        public void JsonMessageSerializer_WhenGivenNullTypeResolver_ThrowsArgumentNullException() {
+            // given
+            IMessageTypeResolver resolver = null;
+            TextWriter textWriter = new StringWriter();
+
+            // when
+            ArgumentNullException exception =
+                Assert.Throws<ArgumentNullException>(
+                    () => new JsonMessageSerializer(textWriter, resolver));
+
+            // then
+            Assert.That(() => exception.ParamName, Is.EqualTo("typeResolver"));
+        }
+
+        [Test]
+        public void JsonMessageSerializer_WhenGivenNullTextWriter_ThrowsArgumentNullException() {
+            // given
+            IMessageTypeResolver resolver = new UnitTestMessageTypeResolver();
+            TextWriter textWriter = null;
+
+            // when
+            ArgumentNullException exception =
+                Assert.Throws<ArgumentNullException>(
+                    () => new JsonMessageSerializer(textWriter, resolver));
+
+            // then
+            Assert.That(() => exception.ParamName, Is.EqualTo("textWriter"));
+        }
+    }
+}
