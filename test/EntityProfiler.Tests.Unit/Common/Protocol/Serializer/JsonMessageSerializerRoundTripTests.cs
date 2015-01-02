8483f3ad5c75f367c37bfe00e638385eae1c39f4
Sebazzz <triadsebas@gmail.com>
2015-01-02 23:10:33 +0100

    Add test for message corruption

--- test/EntityProfiler.Tests.Unit/Common/Protocol/Serializer/JsonMessageSerializerRoundTripTests.cs
+++ test/EntityProfiler.Tests.Unit/Common/Protocol/Serializer/JsonMessageSerializerRoundTripTests.cs
@@ -1,4 +1,5 @@
 ﻿namespace EntityProfiler.Tests.Unit.Common.Protocol.Serializer {
+    using System;
     using System.IO;
     using System.Text;
     using EntityProfiler.Common.Protocol.Serializer;
@@ -23,6 +24,22 @@
             // then
             Assert.That(() => deserializedMessage.Prop1, Is.EqualTo(name));
             Assert.That(() => deserializedMessage.Prop2, Is.EqualTo(value));
+        }
+
+        [Test]
+        public void JsonMessageSerializer_OnCorruptIncomingJson_ThrowsMessageException() {
+            // given
+            StringBuilder data = new StringBuilder();
+            CreateSerializer(data).SerializeMessage(GenerateFakeMessage());
+            data.Remove(data.Length/2, 10); // at a random position, remove some chars to corrupt the message
+
+            // when / then
+            Assert.Throws<MessageTransferException>(() =>
+                CreateDeserializer(data).DeserializeMessage());
+        }
+
+        private static FakeMessage GenerateFakeMessage() {
+            return new FakeMessage(DateTime.UtcNow.ToFileTimeUtc().ToString("x4"), (int) unchecked(DateTime.UtcNow.ToFileTime() % 1024));
         }
 
         private static JsonMessageSerializer CreateSerializer(StringBuilder stringBuilder) {
