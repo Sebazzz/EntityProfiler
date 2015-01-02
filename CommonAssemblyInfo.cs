a24aa28c86db524a468ed35a4fab21dfcca88f57
Sebazzz <triadsebas@gmail.com>
2015-01-02 20:33:34 +0100

    Initial set-up of library structure

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("")]
+#endif
+
+[assembly: AssemblyCompany("Entity Profiler initiative")]
+[assembly: AssemblyProduct("Entity Profiler")]
+[assembly: AssemblyCopyright("Copyright © Sebastiaan Dammann and contributors 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
