a56b926e863a9e750c75a3891c08a63eff7356ce
Sebazzz <triadsebas@gmail.com>
2015-01-03 20:47:50 +0100

    Add integration test for IPC

---
+++ test/EntityProfiler.Tests.Integration/Protocol/FakeMessage.cs
@@ -0,0 +1,22 @@
+﻿namespace EntityProfiler.Tests.Integration.Protocol {
+    using EntityProfiler.Common.Protocol;
+
+    public class FakeMessage : Message {
+        public string Prop1 { get; set; }
+
+        public int Prop2 { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// </summary>
+        public FakeMessage() {}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// </summary>
+        public FakeMessage(string prop1, int prop2) {
+            this.Prop1 = prop1;
+            this.Prop2 = prop2;
+        }
+    }
+}
---
+++ test/EntityProfiler.Tests.Integration/Protocol/MessageRoundTripTests.cs
@@ -0,0 +1,64 @@
+﻿namespace EntityProfiler.Tests.Integration.Protocol {
+    using Common.Events;
+    using Common.Protocol.Serializer;
+    using Interceptor.Protocol;
+    using Interceptor.Reader.Protocol;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class MessageRoundTripTests {
+        private IMessageSink _messageSink;
+        private IMessageListener _messageListener;
+        private DelegateMessageEventSubscriber _eventSubscriber;
+        private MessageEventDispatcher _messageEventDispatcher;
+
+        [TestFixtureSetUp]
+        public void FixtureSetup() {
+            this._eventSubscriber = new DelegateMessageEventSubscriber();
+            this._messageEventDispatcher = new MessageEventDispatcher(new[]{this._eventSubscriber});
+            this._messageSink = new TcpMessageSink(
+                new TcpListenerFactory(),
+                this._messageEventDispatcher,
+                new JsonMessageSerializerFactory(new UnitTestMessageTypeResolver()));
+            this._messageSink.Start();
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureShutdown() {
+            this._messageSink.Dispose();
+        }
+
+        [SetUp]
+        public void TestSetup() {
+            this._messageListener = new TcpMessageListener(
+                new TcpClientFactory(), new JsonMessageDeserializerFactory(new UnitTestMessageTypeResolver()), this._messageEventDispatcher);
+        }
+
+        [TearDown]
+        public void TestTearDown() {
+            this._eventSubscriber.Reset();
+            this._messageListener.Dispose();
+        }
+
+        [Test]
+        public void MessageRoundTrip_SimpleTest() {
+            // given
+            FakeMessage sentMessage = new FakeMessage("test1", 1337);
+            this._messageListener.Start();
+
+            // when
+            this._messageSink.DispatchMessage(sentMessage);
+
+            // then
+            MessageEvent messageEvent = this._eventSubscriber.GetReceivedMessage(100);
+
+            Assert.IsNull(messageEvent.Exception, "Exception occurred during receiving of messages");
+            FakeMessage receivedMessage = (FakeMessage) messageEvent.Message;
+
+            Assert.That(() => receivedMessage.Prop1, Is.EqualTo(sentMessage.Prop1));
+            Assert.That(() => receivedMessage.Prop2, Is.EqualTo(sentMessage.Prop2));
+
+            this._eventSubscriber.AssertNoMessagesReceived();
+        }
+    }
+}
