49787331e27c7b289fe8c6585f92b27a54026567
Sebazzz <triadsebas@gmail.com>
2015-01-08 21:17:23 +0100

    Print received text in status bar

--- src/EntityProfiler.UI/ViewModels/ShellViewModel.cs
+++ src/EntityProfiler.UI/ViewModels/ShellViewModel.cs
@@ -1,28 +1,59 @@
 ﻿namespace EntityProfiler.UI.ViewModels {
     using System;
+    using System.Windows.Threading;
     using Caliburn.Micro;
     using Common.Events;
     using Common.Protocol;
     using Interceptor.Reader.Protocol;
     using PropertyChanged;
+    using Services;
 
     [ImplementPropertyChanged]
     public class ShellViewModel : Screen, IShell, IHandle<MessageEvent> {
         private readonly IMessageListener _messageListener;
 
         /// <summary>
-        /// Creates an instance of the screen.
+        ///     Creates an instance of the screen.
         /// </summary>
         public ShellViewModel(IMessageListener messageListener, IEventAggregator eventAggregator) {
             this._messageListener = messageListener;
-            
+
             eventAggregator.Subscribe(this);
+        }
+
+        /// <summary>
+        ///     Creates an instance of the screen.
+        /// </summary>
+        [Obsolete("This is a design-time only constructor")]
+        public ShellViewModel() {
+            this.StatusBar = "Connected";
         }
 
         public string StatusBar { get; set; }
 
         /// <summary>
-        /// Called when initializing.
+        ///     Handles the message.
+        /// </summary>
+        /// <param name="event">The message.</param>
+        public void Handle(MessageEvent @event) {
+            if (@event.Exception != null) {
+                this.HandleError(@event.Exception);
+                return;
+            }
+
+            var queryMessage = @event.Message as QueryMessage;
+            if (queryMessage != null) {
+                this.HandleQueryMessage(queryMessage);
+            }
+
+            var connectedMessage = @event.Message as ConnectedMessage;
+            if (connectedMessage != null) {
+                this.HandleConnectedMessage(connectedMessage);
+            }
+        }
+
+        /// <summary>
+        ///     Called when initializing.
         /// </summary>
         protected override void OnInitialize() {
             base.OnInitialize();
@@ -34,40 +65,11 @@
         }
 
         /// <summary>
-        /// Creates an instance of the screen.
-        /// </summary>
-        [Obsolete("This is a design-time only constructor")]
-        public ShellViewModel() {
-            this.StatusBar = "Connected";
-        }
-
-        /// <summary>
-        /// Notifies subscribers of the property change.
+        ///     Notifies subscribers of the property change.
         /// </summary>
         /// <param name="propertyName">Name of the property.</param>
-        public sealed override void NotifyOfPropertyChange(string propertyName = null) {
+        public override sealed void NotifyOfPropertyChange(string propertyName = null) {
             base.NotifyOfPropertyChange(propertyName);
-        }
-
-        /// <summary>
-        /// Handles the message.
-        /// </summary>
-        /// <param name="event">The message.</param>
-        public void Handle(MessageEvent @event) {
-            if (@event.Exception != null) {
-                this.HandleError(@event.Exception);
-                return;
-            }
-
-            QueryMessage queryMessage = @event.Message as QueryMessage;
-            if (queryMessage != null) {
-                this.HandleQueryMessage(queryMessage);
-            }
-
-            ConnectedMessage connectedMessage = @event.Message as ConnectedMessage;
-            if (connectedMessage != null) {
-                this.HandleConnectedMessage(connectedMessage);
-            }
         }
 
         private void HandleConnectedMessage(ConnectedMessage connectedMessage) {
@@ -78,9 +80,11 @@
             this.StatusBar = exception.GetType().FullName;
         }
 
-        private int _msgCount = 0;
+        private OneTimeAction _resetStatusBarAction;
         private void HandleQueryMessage(QueryMessage queryMessage) {
-            this.StatusBar = this._msgCount++ + " messages received (" + queryMessage.GetType().FullName + ")";
+            this.StatusBar = queryMessage.Query.CommandText;
+
+            this._resetStatusBarAction = OneTimeAction.Execute(750, () => this.StatusBar = "Ready").CancelExisting(this._resetStatusBarAction);
         }
     }
 }
